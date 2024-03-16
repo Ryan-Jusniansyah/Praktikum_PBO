f0487b55315757e2107d258b27b6787a03800f93
J_Ryan <114414641+Ryan-Jusniansyah@users.noreply.github.com>
2024-03-17 00:51:50 +0800

    Add files via upload

---
+++ Posttest2/src/Posttest2/akun.java
@@ -0,0 +1,20 @@
+package Posttest2;
+
+class akun {
+    private String Username;
+    private String Password;
+
+    akun(String Id, String pass){
+        this.Username = Id;
+        this.Password = pass;
+    }
+    
+    public String getuser(){
+        return Username;
+    }
+
+    public String getpass(){
+        return Password;
+    }
+
+}
